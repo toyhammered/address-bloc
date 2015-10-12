02b80c3f332e90f74779bc8f153a3378645dd3b1
toyhammered <daniel@rassiner.com>
2015-10-11 20:59:37 -0700

    completed section 21, models

--- spec/entry_spec.rb
+++ spec/entry_spec.rb
@@ -1,4 +1,3 @@
-#----- Testing Command ---
 # rspec spec/entry_spec.rb
 
 require_relative 'spec_helper'
@@ -9,18 +8,28 @@ RSpec.describe Entry do
 
 	describe "attributes" do
 		it "should respond to name" do
-			entry = Entry.new
+			entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.King@lovelace.com')
 			expect(entry).to respond_to(:name)
 		end
 
 		it "should respond to phone number" do
-			entry = Entry.new
+			entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.King@lovelace.com')
 			expect(entry).to respond_to(:phone_number)
 		end
 
 		it "should respond to email" do
-			entry = Entry.new
+			entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.King@lovelace.com')
 			expect(entry).to respond_to(:email)
+		end
+	end
+
+
+	# the '#' in front of to_s indicates that it is an instance method
+	describe "#to_s" do
+		it "prints an entry as a string" do
+			entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.King@lovelace.com')
+			expected_string = "Name: Ada Lovelace\nPhone Number: 010.012.1815\nEmail: augusta.King@lovelace.com"
+			expect(entry.to_s).to eq(expected_string)
 		end
 	end
 
