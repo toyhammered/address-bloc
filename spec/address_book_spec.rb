ff476d720107a310bd3f5cd244d82c7d19e16e74
toyhammered <daniel@rassiner.com>
2015-10-13 17:02:29 -0700

    completed assignemtn 21 models

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -42,4 +42,23 @@ RSpec.describe AddressBook do
 		end
 	end
 
+
+	describe "#remove_entry" do
+		it "it removes an entry using the name, phone_number, and email address" do
+			book = AddressBook.new
+			book.add_entry('Daniel Rassiner', '123.456.7890', 'daniel@rassiner.com')
+
+			name = 'Ada Lovelace'
+			phone_number = '010.012.1815'
+			email = 'augusta.king@lovelace.com'
+			book.add_entry(name, phone_number, email)
+			expect(book.entries.size).to eq(2)
+
+			book.remove_entry(name, phone_number, email)
+			expect(book.entries.size).to eq(1)
+			expect(book.entries.first.name).to eq("Daniel Rassiner")
+
+		end
+	end
+
 end
